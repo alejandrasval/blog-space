fa3d17fb1a878fc6d993fb50fad498538a23b893
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2022-07-27 22:37:26 -0500

    script.js update and refactor

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    const postsArr = data.slice(0, 10);
+let postArr = [];
+let title = document.getElementById("post-title");
+let body = document.getElementById("post-body");
+const form = document.getElementById("new-post");
+const articles = document.getElementById("articles");
 
-    let html = "";
-
-    for (let post of postsArr) {
-      html += `
+function renderPosts() {
+  let html = "";
+  for (let post of postArr) {
+    html += `
             <div class="article">
                 <h2>${post.title}</h2>
                 <p>${post.body}</p>
                 <hr class="line-through">
             </div>
-            `;
-    }
+        `;
+  }
+  articles.innerHTML = html;
+}
 
-    const articles = document.getElementById("articles");
-    articles.innerHTML = html;
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    postArr = data.slice(0, 10);
+    renderPosts();
   });
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const titlePost = title.value;
+  const bodyPost = body.value;
+  const data = {
+    title: titlePost,
+    body: bodyPost,
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      postArr.unshift(data);
+      renderPosts();
+      form.reset();
+    });
+});
